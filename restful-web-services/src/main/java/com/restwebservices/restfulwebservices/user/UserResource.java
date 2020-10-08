487690e12eec102d33e44529ec5e86b8c8b51418
kundan keshidi <kundankeshidik@gmail.com>
2020-10-07 21:38:52 -0700

    Enhancing POST method to return correct HTTP status code

--- restful-web-services/src/main/java/com/restwebservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/restwebservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.restwebservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -15,22 +18,31 @@ public class UserResource {
 	@Autowired
 	private UserDaoService service;
 	// retrieve all users
-	
+
 	@GetMapping(path = "/users")
-	public List<User> retrieveAll(){
+	public List<User> retrieveAll() {
 		return service.findAll();
 	}
-	
+
 	// retrieveUser(int id)
 	@GetMapping(path = "/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
-	
+
 	// create a user
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
+		// CREATED
+		// /user/4
+		
+		URI location = ServletUriComponentsBuilder
+		       .fromCurrentRequest()
+		       .path("/{id}")
+		       .buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
-	
+
 }
